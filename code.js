2ca041217d639376c3a9f1b3b59e2486b9cfaf94
KhanSahil07 <adnanshaokh9@gmail.com>
2025-08-25 20:24:46 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,240 @@
+
+const countryList = {
+
+  AED: "AE",
+  AFN: "AF",
+  XCD: "AG",
+  ALL: "AL",
+  AMD: "AM",
+  ANG: "AN",
+  AOA: "AO",
+  AQD: "AQ",
+  ARS: "AR",
+  AUD: "AU",
+  AZN: "AZ",
+  BAM: "BA",
+  BBD: "BB",
+  BDT: "BD",
+  XOF: "BE",
+  BGN: "BG",
+  BHD: "BH",
+  BIF: "BI",
+  BMD: "BM",
+  BND: "BN",
+  BOB: "BO",
+  BRL: "BR",
+  BSD: "BS",
+  NOK: "BV",
+  BWP: "BW",
+  BYR: "BY",
+  BZD: "BZ",
+  CAD: "CA",
+  CDF: "CD",
+  XAF: "CF",
+  CHF: "CH",
+  CLP: "CL",
+  CNY: "CN",
+  COP: "CO",
+  CRC: "CR",
+  CUP: "CU",
+  CVE: "CV",
+  CYP: "CY",
+  CZK: "CZ",
+  DJF: "DJ",
+  DKK: "DK",
+  DOP: "DO",
+  DZD: "DZ",
+  ECS: "EC",
+  EEK: "EE",
+  EGP: "EG",
+  ETB: "ET",
+  EUR: "FR",
+  FJD: "FJ",
+  FKP: "FK",
+  GBP: "GB",
+  GEL: "GE",
+  GGP: "GG",
+  GHS: "GH",
+  GIP: "GI",
+  GMD: "GM",
+  GNF: "GN",
+  GTQ: "GT",
+  GYD: "GY",
+  HKD: "HK",
+  HNL: "HN",
+  HRK: "HR",
+  HTG: "HT",
+  HUF: "HU",
+  IDR: "ID",
+  INR: "IN",
+  IQD: "IQ",
+  IRR: "IR",
+  ISK: "IS",
+  JMD: "JM",
+  JOD: "JO",
+  JPY: "JP",
+  KES: "KE",
+  KGS: "KG",
+  KHR: "KH",
+  KMF: "KM",
+  KPW: "KP",
+  KRW: "KR",
+  KWD: "KW",
+  KYD: "KY",
+  KZT: "KZ",
+  LAK: "LA",
+  LBP: "LB",
+  LKR: "LK",
+  LRD: "LR",
+  LSL: "LS",
+  LTL: "LT",
+  LVL: "LV",
+  LYD: "LY",
+  MAD: "MA",
+  MDL: "MD",
+  MGA: "MG",
+  MKD: "MK",
+  MMK: "MM",
+  MNT: "MN",
+  MOP: "MO",
+  MRO: "MR",
+  MTL: "MT",
+  MUR: "MU",
+  MVR: "MV",
+  MWK: "MW",
+  MXN: "MX",
+  MYR: "MY",
+  MZN: "MZ",
+  NAD: "NA",
+  XPF: "NC",
+  NGN: "NG",
+  NIO: "NI",
+  NPR: "NP",
+  NZD: "NZ",
+  OMR: "OM",
+  PAB: "PA",
+  PEN: "PE",
+  PGK: "PG",
+  PHP: "PH",
+  PKR: "PK",
+  PLN: "PL",
+  PYG: "PY",
+  QAR: "QA",
+  RON: "RO",
+  RSD: "RS",
+  RUB: "RU",
+  RWF: "RW",
+  SAR: "SA",
+  SBD: "SB",
+  SCR: "SC",
+  SDG: "SD",
+  SEK: "SE",
+  SGD: "SG",
+  SKK: "SK",
+  SLL: "SL",
+  SOS: "SO",
+  SRD: "SR",
+  STD: "ST",
+  SVC: "SV",
+  SYP: "SY",
+  SZL: "SZ",
+  THB: "TH",
+  TJS: "TJ",
+  TMT: "TM",
+  TND: "TN",
+  TOP: "TO",
+  TRY: "TR",
+  TTD: "TT",
+  TWD: "TW",
+  TZS: "TZ",
+  UAH: "UA",
+  UGX: "UG",
+  USD: "US",
+  UYU: "UY",
+  UZS: "UZ",
+  VEF: "VE",
+  VND: "VN",
+  VUV: "VU",
+  YER: "YE",
+  ZAR: "ZA",
+  ZMK: "ZM",
+  ZWD: "ZW",
+};
+
+const baseUrl="https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+
+// Get all select elements inside elements with class "dropdown"
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+const btn=document.querySelector("form button")
+
+const fromCurr=document.querySelector(".from select")
+
+const toCurr=document.querySelector(".to select")
+
+const msgs=document.querySelector(".msg")
+
+document.getElementById("refreshBtn").addEventListener("click", () => {
+      location.reload(); // reloads the page
+    });
+
+
+
+// Fill each dropdown with options
+dropdowns.forEach(select => {
+    for (let currCode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode; // Visible text
+        newOption.value = currCode;     // Value
+        select.append(newOption);
+    
+         if(select.name==="from" && currCode==="USD"){
+newOption.selected="selected"
+         }
+        else if(select.name==="to" && currCode==="INR"){
+newOption.selected="selected"
+    
+  }
+  }
+
+//Stop pagce refresh on submit
+
+select.addEventListener("change",(evt) =>
+    {
+updateFlag(evt.target)
+  });
+  });
+const updateFlag=(element)=>
+{
+let currCode=element.value;
+
+let countryCode=countryList[currCode];
+ let newSrc=`https://flagsapi.com/${countryCode}/flat/64.png`;
+ let img=element.parentElement.querySelector("img");
+ img.src=newSrc;
+  
+}
+btn.addEventListener("click", async(evt) =>{
+evt.preventDefault();
+let amount=document.querySelector("form input");
+let amt=amount.value;
+
+if(amt=="" || amt<1){
+  amt=1;
+  amount.value="1";
+}
+console.log(fromCurr.value,toCurr.value);
+
+const URL=`${baseUrl}/${fromCurr.value.toLowerCase()}.json`;
+;
+
+ let response = await fetch(URL);
+ let data=await response.json();
+let rate=data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+console.log(rate);
+console.log(amount.value);
+
+   let finalAmount=(amount.value)*rate;
+   msgs.innerText=`${amt} ${fromCurr.value} =${finalAmount} ${toCurr.value}`
+  
+});
